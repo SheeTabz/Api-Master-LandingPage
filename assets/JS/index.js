96e958e6c6b2ed78f98676ca57c2af4986d38e72
SheeTabz <wmuriithitabitha@gmail.com>
2022-09-04 17:02:40 +0300

    Add the result section

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', (e) => {
     method: 'GET',})
 .then(response => response.json())
 .then(data => {
-    
+
 })
 
    
@@ -15,3 +15,10 @@ form.addEventListener('submit', (e) => {
 })
 
 // console.log(data.result.full_short_link)
+{/* <div class="result">
+  <label for=""> <a href="">https://www.frontendmentor.io/challenges/url-shortening-api-landing-page-2ce3ob-G</a>  </label>
+  <div class="resLeft">
+  <label for="" class="short">  <a href="">https://www.frontendmentor.io/challenges</a> </label>
+  <button class="copy">Copy</button>
+</div>
+</div> */}
